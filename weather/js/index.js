e733a575dd1ae760bf67fedc2d43dee11e5185a2
chengzichao <chengzichao@lansheng8.com>
2018-05-16 12:26:23 +0800

    样式修改,以及将返回的数据对象格式化

--- weather/js/index.js
+++ weather/js/index.js
@@ -10,13 +10,40 @@ let formatPar = urlPar => {
   for(let key in urlPar) {
     par.push(key + '=' + urlPar[key]);
   }
-  return '?' + par.join('&');
+  return par.join('&');
 }
-const url = baseURL + formatPar(urlPar);
-const req = new Request(url, {
-  mode: 'no-cors',
-});
-fetch(req)
-.then(res => {
-  console.log(res);
-})
+
+let script = document.createElement('script');
+script.src = baseURL + '?' + formatPar(urlPar) + '&callback=callback';
+document.querySelector('body').appendChild(script);
+
+function callback(res) {
+  let arr = [];
+  for( let prop in res.result.future) {
+    let date = prop.split("_")[1].split("").map((item, index) => {
+      if(index === 3) {
+        return item + '年'
+      }
+      if(index === 4 && item === '0') {
+        return;
+      }
+      if(index === 5) {
+        return item + '月';
+      }
+      if(index === 6 && item === '0') {
+        return;
+      }
+      if(index === 7) {
+        return item + '日';
+      }
+      return item;
+    }).join("");
+    res.result.future[prop].date = date;
+    arr.push(res.result.future[prop]);
+  }
+  res.result.future = arr;
+  const tpl = document.querySelector("#tpl").innerHTML;
+  const html = template(tpl, {data: res.result});
+  document.querySelector(".content").innerHTML = html;
+  document.body.removeChild(script);
+}
