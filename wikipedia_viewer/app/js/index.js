c39b5714ea520e267e106d0f57c0299f63cc7749
chengzichao <592292495@qq.com>
2018-05-21 12:30:17 +0800

    完成搜索功能

--- wikipedia_viewer/app/js/index.js
+++ wikipedia_viewer/app/js/index.js
@@ -2,25 +2,25 @@ let btnSearch = document.querySelector(".shell");
 let input = document.querySelector(".search");
 let line = document.querySelector(".line");
 let close = document.querySelector(".close");
-// let xhr = new XMLHttpRequest();
-// let req = new Request(url)
+// 搜索的api地址
 const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=";
+//点击搜索按钮展开搜索框
 btnSearch.addEventListener("click", () => {
   btnSearch.style.width = "300px";
   line.style.top = "5px";
   line.style.right = "15px";
+  line.style.opacity = 0;
   close.style.display = "block";
   close.style.opacity = 1;
   setTimeout(() => {
     input.readOnly = false;
   }, 300);
 });
-// btnSearch.addEventListener("transitionend", () => {
-//   if(btnSearch.style.width = "300px") {
-//     input.readOnly = false;
-//   }
-// })
+// 点击关闭时关闭搜索框
 close.addEventListener("click", e => {
+  document.querySelector('.res-list').innerHTML = '';
+  document.querySelector('.res-list').style.display = 'none';
+  document.querySelector('.explain').style.display = 'block';
   e.cancelBubble = true;
   btnSearch.style.width = "40px";
   close.style.display = "none";
@@ -28,16 +28,35 @@ close.addEventListener("click", e => {
   input.value = "";
   input.readOnly = true;
   setTimeout(() => {
+    line.style.opacity = 1;
     line.style.top = "15px";
     line.style.right = "5px";
   }, 300);
 });
+// 创建script标签，发送jsonp请求
+function jsonp(link, callback) {
+  let script = document.createElement('script');
+  script.id = 'jsonp';
+  script.src = link + '&callback=' + callback;
+  document.querySelector('body').appendChild(script);
+}
+// 请求回调
+function callback(res) {
+  document.querySelector('.explain').style.display = "none";
+  document.querySelector('.res-list').style.display = 'block';
+  let html = ""
+  for(let i = 0; i < res[1].length; i++) {
+    html += `<li class="res-list-item"><a target="_blank" href="${res[3][i]}"><h3>${res[1][i]}</h3><p>${res[2][i].split('.')[0]}.</p></a></li>`
+  }
+  document.querySelector(".res-list").innerHTML = html;
+  document.querySelector('body').removeChild(document.querySelector('#jsonp'));//成功后删除标签
+}
 
 input.addEventListener("keydown", e => {
-  if(e.keyCode === 13) {
-     let link = url + input.value;
-    fetch(link).then(res => {
-      console.log(res)
-    })
+  if (e.keyCode === 13) {
+    let link = url + input.value;
+    jsonp(link, 'callback')
   }
-}) 
+})
+
+
--- wikipedia_viewer/gulpfile.js
+++ wikipedia_viewer/gulpfile.js
@@ -1,8 +1,29 @@
 const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
 const sass = require('gulp-sass');
 gulp.task('sass', () => {
   return gulp.src('app/scss/*.scss')
   .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
   .pipe(gulp.dest('app/css/'))
+  .pipe(reload({stream: true}));
 })
-gulp.watch('app/scss/*.scss', ['sass']);
+gulp.task('html', () => {
+  return gulp.src('app/*.html')
+  .pipe(reload({stream: true}));
+})
+gulp.task('js', () => {
+  return gulp.src('app/js/*.js')
+  .pipe(reload({stream: true}));
+})
+gulp.task('server', ['sass'], () => {
+  browserSync({
+    server: {
+      baseDir: './app/'
+    }
+  });
+
+  gulp.watch(['app/scss/*.scss'], ['sass']);
+  gulp.watch(['app/js/*.js'], ['js']);
+  gulp.watch(['app/*.html'], ['html']);
+})
